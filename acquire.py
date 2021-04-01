e016b1a9b7c258dbfd91056be5b77d260721aa6a
Gabby Broussard <gabriella.b.broussard@gmail.com>
2021-04-01 15:57:35 -0500

    done[

---
+++ acquire.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import pandas as pd
+import numpy as np
+import os
+import env
+
+
+# In[ ]:
+
+
+# creating a connection to connect to the Codeup Student Database
+def get_connection(db, user=env.user, host=env.host, password=env.password):
+    return f'mysql+pymysql://{user}:{password}@{host}/{db}'
+
+
+# In[ ]:
+
+
+def get_zillow_data():
+    '''This function will connect to the Codeup Student Database. It will then cache a local copy to the computer to use for later
+        in the form of a CSV file. If you want to reproduce the results, you will need your own env.py file and database credentials.'''
+    filename = "zillow_db.csv"
+    if os.path.isfile(filename):
+        return pd.read_csv(filename)
+    else:
+        # read the SQL query into a dataframe
+        df = pd.read_sql('''
+SELECT prop.*, 
+       pred.logerror, 
+       pred.transactiondate, 
+       air.airconditioningdesc, 
+       arch.architecturalstyledesc, 
+       build.buildingclassdesc, 
+       heat.heatingorsystemdesc, 
+       landuse.propertylandusedesc, 
+       story.storydesc, 
+       construct.typeconstructiondesc 
+FROM   properties_2017 prop  
+       INNER JOIN (SELECT parcelid,
+                          logerror,
+                          Max(transactiondate) transactiondate 
+                   FROM   predictions_2017 
+                   GROUP  BY parcelid, logerror) pred
+               USING (parcelid) 
+       LEFT JOIN airconditioningtype air USING (airconditioningtypeid) 
+       LEFT JOIN architecturalstyletype arch USING (architecturalstyletypeid) 
+       LEFT JOIN buildingclasstype build USING (buildingclasstypeid) 
+       LEFT JOIN heatingorsystemtype heat USING (heatingorsystemtypeid) 
+       LEFT JOIN propertylandusetype landuse USING (propertylandusetypeid) 
+       LEFT JOIN storytype story USING (storytypeid) 
+       LEFT JOIN typeconstructiontype construct USING (typeconstructiontypeid) 
+WHERE  prop.latitude IS NOT NULL 
+       AND prop.longitude IS NOT NULL;
+            ''' , get_connection('zillow'))
+        # Write that dataframe to disk for later. Called "caching" the data for later.
+        df.to_csv(filename)
+        # Return the dataframe to the calling code
+        return df
